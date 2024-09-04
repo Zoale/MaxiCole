2a23b8b535459b71bb9843ccb4e1504a901946ba
ZoAle <zoiloguzmangalanton@gmail.com>
2024-09-04 16:53:10 -0300

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Llama al método main de la clase MongoDB para establecer la conexión
+        MongoDB.main(args);
+
+        // Aquí puedes continuar con la lógica adicional de tu aplicación
+        System.out.println("Conexión a MongoDB realizada. Continuando con la aplicación...");
+    }
+}
